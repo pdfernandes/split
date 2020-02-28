3fd30fa65e8922296ea98238e686f2c66e5ad846
alvin <azablan.dev@gmail.com>
2020-02-28 13:21:48 -0500

    init root component and index.html

---
+++ split-frontend/src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return <div>Welcome to Split!</div>;
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
