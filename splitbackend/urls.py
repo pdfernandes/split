9c5180615d18559a4643054d3175e312d6fc02ef
azablan <azablan.dev@gmail.com>
2020-03-05 18:28:22 -0500

    finish user auth endpoints

--- splitbackend/urls.py
+++ splitbackend/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from rest_framework_simplejwt import views as jwt_views
-from .views import HelloView
+from .views import user_create, Hello
 
 
 urlpatterns = [
     path('token/', jwt_views.TokenObtainPairView.as_view()),
     path('token/refresh/', jwt_views.TokenRefreshView.as_view()),
-    path('hello/', HelloView.as_view())
+    path('hello/', Hello.as_view()),
+    path('user/', user_create)
 ]
