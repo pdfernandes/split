d1d406ac217ef4e891d67d6ed9407fda786f0a35
pfernandes <phelipe.fern@gmail.com>
2020-03-05 14:45:10 -0500

    fix db add __str__ and additional keys/attributes

--- split/settings.py
+++ split/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'splitbackend.apps.SplitbackendConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- splitbackend/models.py
+++ splitbackend/models.py
@@ -1,2 +1,38 @@
 from django.db import models
 
+
+class Profile(models.Model):
+    def __str__(self):
+        return str(self.id)
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+
+class Tab(models.Model):
+    def __str__(self):
+        return self.title
+    owner_id = models.OneToOneField(Profile, on_delete=models.CASCADE, related_name='owner')
+    title = models.CharField(max_length=30)
+    participants = models.ManyToManyField(Profile)
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+
+class Item(models.Model):
+    def __str__(self):
+        return self.name
+    name = models.CharField(max_length=30)
+    cost = models.DecimalField(max_digits=17, decimal_places=3)
+    tab_id = models.ForeignKey(Tab, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+
+class Split(models.Model):
+    def __str__(self):
+        return self.percent
+    profile_id = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    item_id = models.ForeignKey(Item, on_delete=models.CASCADE)
+    percent = models.DecimalField(max_digits=4, decimal_places=3)
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
