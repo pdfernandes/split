a72e58353f756d5374fa3a52e8a13e100166a6eb
pfernandes <phelipe.fern@gmail.com>
2020-03-06 17:04:29 -0500

    add custom validator for item cost and default value for split %

--- splitbackend/models.py
+++ splitbackend/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
 
 class Profile(models.Model):
@@ -22,10 +23,18 @@ class Item(models.Model):
     def __str__(self):
         return self.name
     name = models.CharField(max_length=30)
-    cost = models.DecimalField(max_digits=17, decimal_places=3)
+    cost = models.DecimalField(max_digits=17, decimal_places=2)
     tab = models.ForeignKey(Tab, on_delete=models.CASCADE, related_name='items')
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
+
+    def clean(self):
+        if self.cost < 0:
+            raise ValidationError("Cost must be atleast 0.")
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        return super(Item, self).save(*args, **kwargs)
 
 
 class Split(models.Model):
@@ -33,6 +42,6 @@ class Split(models.Model):
         return self.percent
     profile = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='participated_splits')
     item = models.ForeignKey(Item, on_delete=models.CASCADE, related_name='splits')
-    percent = models.DecimalField(max_digits=4, decimal_places=3)
+    percent = models.DecimalField(max_digits=4, decimal_places=3, default=1)
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
